d7785185f6a9be64919bd492f8c3cbd007df0073
Daniil Kirillov <dikirillov@edu.hse.ru>
2024-09-28 18:00:12 +0300

    collate fn initial version

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 
 
 def collate_fn(dataset_items: list[dict]):
@@ -14,4 +15,38 @@ def collate_fn(dataset_items: list[dict]):
             of the tensors.
     """
 
-    pass  # TODO
+    keys_to_pad = ["audio", "spectrogram", "text_encoded"]
+    keys_to_merge = ["text", "audio_path"]
+    seq_lens = [0 for i in range(len(keys_to_pad))]
+
+    finalized_batch = {}
+    for key in keys_to_pad:
+        finalized_batch[key] = []
+        finalized_batch["{key}_length".format(key=key)] = []
+    for key in keys_to_merge:
+        finalized_batch[key] = []
+
+    for single_item in dataset_items:
+        for index, key in enumerate(keys_to_pad):
+            seq_lens[index] = max(seq_lens[index], single_item[key].shape[-1])
+            finalized_batch["{key}_length".format(key=key)].append(
+                single_item[key].shape[-1]
+            )
+
+    for single_item in dataset_items:
+        for index, key in enumerate(keys_to_pad):
+            finalized_batch[key].append(
+                F.pad(
+                    single_item[key], (0, seq_lens[index] - single_item[key].shape[-1])
+                )
+            )
+        for key in keys_to_merge:
+            finalized_batch[key].append(single_item[key])
+
+    for key in keys_to_pad:
+        finalized_batch[key] = torch.cat(finalized_batch[key])
+        finalized_batch["{key}_length".format(key=key)] = torch.tensor(
+            finalized_batch["{key}_length".format(key=key)]
+        )
+
+    return finalized_batch
